2bba296c877fb09ed1d55c37e2303d7472a6b998
mrwthr <sam.merewether@gmail.com>
2014-09-04 05:00:43 +0800

    init upload
    init upload

---
+++ js/main.js
@@ -0,0 +1,158 @@
+ /**
+ * This demo was prepared for you by Petr Tichy - Ihatetomatoes.net
+ * Want to see more similar demos and tutorials?
+ * Help by spreading the word about Ihatetomatoes blog.
+ * Facebook - https://www.facebook.com/ihatetomatoesblog
+ * Twitter - https://twitter.com/ihatetomatoes
+ * Google+ - https://plus.google.com/u/0/109859280204979591787/about
+ * Article URL: http://ihatetomatoes.net/how-to-make-parallax-website-responsive/
+ */
+
+( function( $ ) {
+	
+	// Setup variables
+	$window = $(window);
+	$slide = $('.homeSlide');
+	$body = $('body');
+	
+    //FadeIn all sections   
+	$body.imagesLoaded( function() {
+		setTimeout(function() {
+		      
+		      // Resize sections
+		      adjustWindow();
+		      
+		      // Fade in sections
+			  $body.removeClass('loading').addClass('loaded');
+			  
+		}, 800);
+	});
+	
+	function adjustWindow(){
+
+	    // Get window size
+	    winH = $window.height();
+	    winW = $window.width();
+
+	    // Keep minimum height 550
+	    if(winH <= 550) {
+	        winH = 550;
+	    }
+
+	    // Init Skrollr for 768 and up
+	    if( winW >= 768) {
+
+	        // Init Skrollr
+	        var s = skrollr.init({
+	            forceHeight: false
+	        });
+
+	        // Resize our slides
+	        $slide.height(winH);
+
+	        s.refresh($('.homeSlide'));
+
+	    } else {
+
+	        // Init Skrollr
+	        var s = skrollr.init();
+	        s.destroy();
+	    }
+	
+		// Check for touch
+	   	if(Modernizr.touch) {
+
+			// Init Skrollr
+			var s = skrollr.init();
+			s.destroy();
+	   	}
+
+	}
+	
+	function initAdjustWindow() {
+	    return {
+	        match : function() {
+	            adjustWindow();
+	        },
+	        unmatch : function() {
+	            adjustWindow();
+	        }
+	    };
+	}
+
+	enquire.register("screen and (min-width : 768px)", initAdjustWindow(), false)
+	        .listen(100);
+		
+} )( jQuery );
+
+//ONSCROLL
+
+function init() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 50,
+            header = document.querySelector("header");
+        if (distanceY > shrinkOn) {
+            classie.add(header,"smaller");
+        } else {
+            if (classie.has(header,"smaller")) {
+                classie.remove(header,"smaller");
+            }
+        }
+    });
+}
+window.onload = init();
+
+
+
+//NAV??
+///////
+jQuery(document).ready(function($){
+	// browser window scroll (in pixels) after which the "menu" link is shown
+	var offset = 0;
+
+	var navigationContainer = $('#cd-nav'),
+		mainNavigation = navigationContainer.find('#cd-main-nav ul');
+
+	//hide or show the "menu" link
+	checkMenu();
+	$(window).scroll(function(){
+		checkMenu();
+	});
+
+	//open or close the menu clicking on the bottom "menu" link
+	$('.cd-nav-trigger').on('click', function(){
+		$(this).toggleClass('menu-is-open');
+		//we need to remove the transitionEnd event handler (we add it when scolling up with the menu open)
+		mainNavigation.off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend').toggleClass('is-visible');
+
+	});
+
+	function checkMenu() {
+		if( $(window).scrollTop() > offset && !navigationContainer.hasClass('is-fixed')) {
+			navigationContainer.addClass('is-fixed').find('.cd-nav-trigger').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+				mainNavigation.addClass('has-transitions');
+			});
+		} else if ($(window).scrollTop() <= offset) {
+			//check if the menu is open when scrolling up
+			if( mainNavigation.hasClass('is-visible')  && !$('html').hasClass('no-csstransitions') ) {
+				//close the menu with animation
+				mainNavigation.addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+					//wait for the menu to be closed and do the rest
+					mainNavigation.removeClass('is-visible is-hidden has-transitions');
+					navigationContainer.removeClass('is-fixed');
+					$('.cd-nav-trigger').removeClass('menu-is-open');
+				});
+			//check if the menu is open when scrolling up - fallback if transitions are not supported
+			} else if( mainNavigation.hasClass('is-visible')  && $('html').hasClass('no-csstransitions') ) {
+					mainNavigation.removeClass('is-visible has-transitions');
+					navigationContainer.removeClass('is-fixed');
+					$('.cd-nav-trigger').removeClass('menu-is-open');
+			//scrolling up with menu closed
+			} else {
+				navigationContainer.removeClass('is-fixed');
+				mainNavigation.removeClass('has-transitions');
+			}
+		} 
+	}
+});
